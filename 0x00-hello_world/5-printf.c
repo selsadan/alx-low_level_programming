9fa8ac647c9d47a294c6d6f94406deac1fdd256d
selsadan <danielfrancis442@gmail.com>
2022-09-09 03:04:10 -0700

    exercise on C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function 
+ * this program prints "with proper grammer, but the outcome is a piece of art"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
+}	
