1975ab4be43b774ce60ac1e1f006378c5d411dc0
selsadan <danielfrancis442@gmail.com>
2022-09-14 03:48:13 -0700

    Fuctions exercise!

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,9 @@ void times_table(void)
 				_putchar(' ');
 
 				prod = num * mult;
-																							if (prod <= 9)
+
+
+				if (prod <= 9)
 					_putchar(' ');
 				else
 					_putchar((prod / 10) + '0');
